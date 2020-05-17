31db5db7a8ecf327a27a0e8f1b49f163b732b398
Maurice Parker <mo@vincode.io>
2020-05-17 11:59:34 -0500

    Change to process as we process the incoming XML instead of accumulating articles before processing them

--- import_categories.py
+++ import_categories.py
@@ -11,7 +11,7 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
         self._buffer = None
         self._values = {}
         self._current_tag = None
-        self._pages = []
+        self._pageCount = 0
 
     def characters(self, content):
         '''Characters between opening and closing tags'''
@@ -30,33 +30,39 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
             self._values[name] = ' '.join(self._buffer)
         
         if name == 'page':
-            self._pages.append((self._values['title'], self._values['text']))
+            self._pageCount = self._pageCount + 1
+            self.processText(self._values['title'], self._values['text'])
 
+    def processText(self, title, text):
+        parsed_article = mwparserfromhell.parse(text)
+        categories = self.extract_categories(parsed_article)
+        print(title)
+        print(categories)
 
-def strip_to_category(category):
-    ''' Strip down to the bare category '''
+    def strip_to_category(self, category):
+        ''' Strip down to the bare category '''
 
-    if category.startswith('[[Category:'):
-        category = category[11:-2]
-    elif category.startswith('Category:'):
-        category = category[9:]
-    return category.split('|')[0]
+        if category.startswith('[[Category:'):
+            category = category[11:-2]
+        elif category.startswith('Category:'):
+            category = category[9:]
+        return category.split('|')[0]
 
-def extract_categories(parsed_article):
-    ''' Extract the sub categories from a category page '''
+    def extract_categories(self, parsed_article):
+        ''' Extract the sub categories from a category page '''
 
-    wikilinks = parsed_article.filter_wikilinks()
-    categories = []
-    for wikilink in wikilinks:
-        if wikilink.startswith('[[Category:'):
-            categories.append(strip_to_category(wikilink))
+        wikilinks = parsed_article.filter_wikilinks()
+        categories = []
+        for wikilink in wikilinks:
+            if wikilink.startswith('[[Category:'):
+                categories.append(self.strip_to_category(wikilink))
 
-    return categories
+        return categories
 
 
 def process(wikifile):
     ''' Process a wikipedia dump file '''
-    
+
     handler = WikiXmlHandler()
 
     parser = xml.sax.make_parser()
@@ -64,16 +70,9 @@ def process(wikifile):
 
     for i, line in enumerate(subprocess.Popen(['bzcat'], stdin = open(wikifile), stdout = subprocess.PIPE).stdout):
         parser.feed(line)
-        if len(handler._pages) > 50:
+        if handler._pageCount > 50:
             break
     
-    parsed_article = mwparserfromhell.parse(handler._pages[2][1])
-    categories = extract_categories(parsed_article)
-    print(handler._pages[2][0])
-    print(categories)
-    #parsed_links = [x.title for x in parsed_article.filter_wikilinks()]
-    #print(f'There are {len(parsed_links)} links.')
-
 process('wikipedia_data/enwiki-20200501-pages-articles26.xml-p42567203p42663461.bz2')
 
 #if __name__ == '__main__': 
