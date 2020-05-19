b6fa90a83e3d4920ba329e8954959523f4b468ea
Maurice Parker <mo@vincode.io>
2020-05-19 08:06:25 -0500

    Enhance main categories query

--- database_queries/find_main_categories.sql
+++ database_queries/find_main_categories.sql
@@ -1,12 +1,14 @@
-WITH RECURSIVE subcategories(id, name, artice_count, depth, path, cycle) AS (
-		SELECT id, name, article_count, 0 depth, ARRAY[id], false
+WITH RECURSIVE graph(id, name, artice_count, depth, path, cycle) AS (
+		SELECT id, '', article_count, 0 depth, ARRAY[id], false
 		FROM fs_category
 		WHERE name = 'main topic classifications'
 	UNION
-		SELECT c.id, c.name, c.article_count, sc.depth + 1, path || c.id, c.id = ANY(path)
+		SELECT c.id, g.name || ' / ' || c.name, c.article_count, g.depth + 1, path || c.id, c.id = ANY(path)
 		FROM fs_category c
 		INNER JOIN fs_category_relationship cr on c.id = cr.fs_category_child_id
-		INNER JOIN subcategories sc ON sc.id = cr.fs_category_parent_id
-		WHERE NOT cycle and sc.depth < 2
+		INNER JOIN graph g ON g.id = cr.fs_category_parent_id
+		WHERE NOT cycle and g.depth < 2
 )
-SELECT * FROM subcategories
+SELECT name FROM graph
+WHERE depth > 1
+order by name
