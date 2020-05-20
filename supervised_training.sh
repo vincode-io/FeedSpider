076da740acd7e3e200153b18cec2151147627f75
Maurice Parker <mo@vincode.io>
2020-05-20 15:54:07 -0500

    Add supervised training script

---
+++ supervised_training.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+WORKING_DIR="working_dir"
+
+echo "Training the model..."
+fasttext supervised -input $WORKING_DIR/train.txt -output $WORKING_DIR/trained_model -lr 1.0 -epoch 25 -wordNgrams 2
+
+echo
+echo "Validating the model..."
+fasttext test $WORKING_DIR/trained_model.bin $WORKING_DIR/valid.txt
