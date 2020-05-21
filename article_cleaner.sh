3f88c7416267431503a03e9893b6217bc41678c2
Maurice Parker <mo@vincode.io>
2020-05-21 17:48:11 -0500

    Change some commands to run faster

--- article_cleaner.sh
+++ article_cleaner.sh
@@ -10,13 +10,18 @@ fi
  cat extracted_articles/* | \
      sed -e "s/\([.\!?,'/()]\)/ \1 /g" | \
      tr "[:upper:]" "[:lower:]" | \
-     sort -R > \
+     shuf > \
      $WORKING_DIR/extracted_articles.txt
 
 # Split the records out into a training group and a validation group
-TOTAL_RECORD_COUNT=`wc -l $WORKING_DIR/extracted_articles.txt | awk '{print $1}'`
-TRAIN_RECORD_COUNT=`echo $TOTAL_RECORD_COUNT | awk '{print int($0 * .8)}'`
-let VALID_RECORD_COUNT=$TOTAL_RECORD_COUNT-$TRAIN_RECORD_COUNT
+awk -v lines=$(wc -l < $WORKING_DIR/extracted_articles.txt) \
+    -v fact=0.80 \
+    'NR <= lines * fact {print > "train.txt"; next} {print > "valid.txt"}' \
+    $WORKING_DIR/extracted_articles.txt
 
-head -n $TRAIN_RECORD_COUNT $WORKING_DIR/extracted_articles.txt > $WORKING_DIR/train.txt
-tail -n $VALID_RECORD_COUNT $WORKING_DIR/extracted_articles.txt > $WORKING_DIR/valid.txt
+# TOTAL_RECORD_COUNT=`wc -l $WORKING_DIR/extracted_articles.txt | awk '{print $1}'`
+# TRAIN_RECORD_COUNT=`echo $TOTAL_RECORD_COUNT | awk '{print int($0 * .8)}'`
+# let VALID_RECORD_COUNT=$TOTAL_RECORD_COUNT-$TRAIN_RECORD_COUNT
+
+# head -n $TRAIN_RECORD_COUNT $WORKING_DIR/extracted_articles.txt > $WORKING_DIR/train.txt
+# tail -n $VALID_RECORD_COUNT $WORKING_DIR/extracted_articles.txt > $WORKING_DIR/valid.txt
