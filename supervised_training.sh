7d915ff6d9cd815dbb2bdc4a69dd796f1091b108
Maurice Parker <mo@vincode.io>
2020-05-25 22:14:46 +0000

    Change training to use 48 threads

--- supervised_training.sh
+++ supervised_training.sh
@@ -3,7 +3,7 @@
 WORKING_DIR="working_dir"
 
 echo "Training the model..."
-fasttext supervised -input $WORKING_DIR/train.txt -output $WORKING_DIR/trained_model -lr 1.0 -epoch 25 -wordNgrams 2
+fasttext supervised -input $WORKING_DIR/train.txt -output $WORKING_DIR/trained_model -lr 1.0 -epoch 25 -wordNgrams 2 -thread 48
 
 echo
 echo "Validating the model..."
